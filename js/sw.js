296e2198ccd33eb10fb41793046c06cc997f9c13
Jose F. Gomez <jgomez352@gmail.com>
2019-06-17 17:28:56 -0400

    add STATIC_CACHE
    added listener to populate static cache

--- js/sw.js
+++ js/sw.js
@@ -41,3 +41,33 @@ function isExternalResource(url) {
     }
     return isExternal;
 };
+
+/* Listeners */
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(STATIC_CACHE).then(function (cache) {
+            console.log("Static cache: ", STATIC_CACHE);
+            return cache.addAll([
+                '/css/styles.css',
+                '/data/restaurants.json',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                '/js/dbhelper.js',
+                '/js/main/js',
+                '/js/restaurant_info.js',
+                '/index.html',
+                '/restaurant.html'
+            ]);
+        })
+    )
+
+});
