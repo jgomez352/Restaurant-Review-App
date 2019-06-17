b696fd9d8b02b6855a718a611d0da17298153c24
Jose F. Gomez <jgomez352@gmail.com>
2019-06-17 16:24:22 -0400

    add service worker
    Added sw.js and static variables

---
+++ js/sw.js
@@ -0,0 +1,12 @@
+/* static */
+
+const CACHE_VERSION = 1;
+const STATIC_CACHE = `static-cache-v${CACHE_VERSION}`;
+const IMAGE_CACHE = `images-cache-v`;
+const OTHER_CACHE = `other-cache-v`;
+const allCaches = [
+    STATIC_CACHE,
+    IMAGE_CACHE,
+    OTHER_CACHE
+];
+
